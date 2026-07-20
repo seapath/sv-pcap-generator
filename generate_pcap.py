6fb1f3b9c02ae3167f9012b10fd23d2f943a124e
Jose Saldana <jmsaldana@fcirce.es>
2026-07-20 17:54:15 +0200

    Update generate_pcap.py
    Signed-off-by: Jose Saldana <jmsaldana@fcirce.es>

--- generate_pcap.py
+++ generate_pcap.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # © 2023 GE Vernova and/or its affiliates. All rights reserved.
+# 2026 improved by Jose Saldana at CIRCE Technology center
 
 import sys
 import struct
@@ -20,6 +21,16 @@ parser.add_argument(
     "-n", "--nb_streams", type=int, help="Number of SV streams", default=64
 )
 
+parser.add_argument(
+    "-m",
+    "--nb_asdu",
+    type=int,
+    help="Number of ASDUs (streams) bundled together in a single frame. "
+    "nb_streams streams are packed into ceil(nb_streams/nb_asdu) frames "
+    "per loop iteration.",
+    default=1,
+)
+
 parser.add_argument("-p", "--svID_prefix", type=str, help="SV ID prefix", default="svID")
 
 parser.add_argument("-d", "--svID_digits", type=int, help="Number of SV ID digits", default=4)
@@ -29,7 +40,7 @@ parser.add_argument(
     "--loop",
     type=int,
     help="Number of iterations."
-    "The cmpCnt field will be increased at each loop",
+    "The smpCnt field will be increased at each loop",
     default=4000,
 )
 
@@ -94,6 +105,7 @@ final_pcap = args.output
 app_id = args.app_id
 start_id = args.start_id
 nb_streams = args.nb_streams
+nb_asdu = args.nb_asdu
 max_counter = args.loop
 i_rms = args.i_rms
 v_rms = args.v_rms
@@ -127,6 +139,16 @@ if nb_streams > svID_max or nb_streams < 1:
     print(f"Error nb_streams must be between 1 and {svID_max}", file=sys.stderr)
     sys.exit(1)
 
+if nb_asdu < 1 or nb_asdu > 255:
+    # NumOfASDU is encoded on a single byte (0x80 0x01 value), so it is
+    # limited to 255 ASDUs per frame.
+    print("Error nb_asdu must be between 1 and 255", file=sys.stderr)
+    sys.exit(1)
+
+if nb_asdu > nb_streams:
+    print("Error nb_asdu must be lower than or equal to nb_streams", file=sys.stderr)
+    sys.exit(1)
+
 if max_counter < 1 or max_counter > 65536:
     print("Error loop must be between 1 and 65536", file=sys.stderr)
     sys.exit(1)
@@ -158,13 +180,26 @@ if vlanPriority < 0 or vlanPriority > 7:
     print("Error VLAN Priority must be between 0 and 7", file=sys.stderr)
     sys.exit(1)
 
-HEADER = (
+PCAP_GLOBAL_HEADER = (
     b"\xd4\xc3\xb2\xa1\x02\x00\x04\x00\x00\x00\x00\x00\x00\x00\x00\x00"
     b"\x00\x00\x04\x00\x01\x00\x00\x00"
 )
 
-start_id_str = f"{start_id:0{nb_digits}d}"
-svIDFirst = svID_prefix + start_id_str
+if vlanID != 0:
+    # 802.1Q VLAN Tag Header
+    # 0x8100: 802.1Q VLAN Tag Protocol Identifier
+    # Priority Code Point (PCP) (3 bits) | CFI (1 bit) | VLAN Identifier (12 bits)
+    tpid = 0x8100
+    tci = (vlanPriority << 13) | vlanID
+    vlan_header = struct.pack("!HH", tpid, tci)
+else:
+    vlan_header = b""
+
+ETHERTYPE_SV = b"\x88\xBA"
+
+CONF_REV_TLV = b"\x83\x04\x00\x00\x00\x01"
+SMP_SYNC_TLV = b"\x85\x01\x00"
+
 
 def get_second_microsecond(ts):
     second = int(ts)
@@ -172,177 +207,138 @@ def get_second_microsecond(ts):
     return (second, microsecond)
 
 
-def write_bytes_le(sv, offset, data, len=-2):
-    for i in data:
-        if len == -1:
-            break
-        sv[offset] = i
-        offset += 1
-        if len > 0:
-            len -= 1
+def ber_length(n):
+    """Encode a length in BER/DER definite form (short or long form)."""
+    if n < 0x80:
+        return bytes([n])
+    length_bytes = n.to_bytes((n.bit_length() + 7) // 8, byteorder="big")
+    return bytes([0x80 | len(length_bytes)]) + length_bytes
 
 
-def write_bytes_be(sv, offset, data):
-    offset += len(data) - 1
-    for i in data:
-        sv[offset] = i
-        offset -= 1
-
-if vlanID != 0:
-    # 802.1Q VLAN Tag Header
-    # 0x8100: 802.1Q VLAN Tag Protocol Identifier
-    # 0x00: Priority Code Point (PCP) (3 bits)
-    # 0x00: Canonical Format Indicator (CFI) (1 bit)
-    # 0x000: VLAN Identifier (VID) (12 bits)
-    tpid = 0x8100
-    tci = (vlanPriority << 13) | vlanID
-    vlan_header = struct.pack("!HH", tpid, tci)
-
-else:
-    vlan_header = b""
-
-vlan_size = len(vlan_header)
-
-# This data are generated from an pcap file
-# It is possible to change the SV to sent. The only restriction is that the
-# SV ID must have 8 bytes lenght and must contains only one ASDU
-# To change this:
-# 1. Capture an SV using Wireshark or tcpdump and generate a pcap file with
-#    times in µs. The file must contained only one SV
-# 2. Drop the pcap header (0 to 0x18)
-# 3. Set timestamp to 0 (set the next 8 bytes to 0)
-# 5. At offset 0x28 Ensure the MAC Address destination is a IEEE 802.1X
-#    Multicast sampled values address (01:0C:CD:04:00:00 to 01:0C:CD:04:01:FF)
-# 6. Adjust in the SV_ID_OFFSET and CMP_CNT_OFFSET in this file
-
-SV_DATA = (
-    # Timestamp
-    b"\x00\x00\x00\x00\x00\x00\x00\x00"
-    # Packet length
-    + bytes([0x70 + len(svIDFirst) + vlan_size]) + b"\x00\x00\x00"
-    # Capture length
-    + bytes([0x70 + len(svIDFirst) + vlan_size]) + b"\x00\x00\x00"
-    # Destination MAC
-    + mac_dest +
-    # Source MAC
-    mac_source +
-    # VLAN Header
-    vlan_header +
-    # Ethertype
-    b"\x88\xBA"
-    # AppId
-    b"\x40\x00"
-    # Length
-    b"\x00" + bytes([0x62 + len(svIDFirst)]) +
-    # Reserved 1 & Reserved 2
-    b"\x00\x00\x00\x00"
-    # savPDU 0x60 Length
-    b"\x60" + bytes([0x58 + len(svIDFirst)]) +
-    # Number of asdu 0x80 L(1) 8
-    b"\x80\x01\x01"
-    # Sequence of asdu 0xA2 L
-    b"\xa2" + bytes([0x53 + len(svIDFirst)]) +
-    # Sequence ASDU1 0x30 L
-    b"\x30" + bytes([0x51 + len(svIDFirst)]) +
-    # SvID 0x80 L Values
-    b"\x80" + bytes([len(svIDFirst)]) + svIDFirst.encode("ascii") +
-    # smpCnt 0x82 L(2) value
-    b"\x82\x02\x00\x00"
-    # ConfRev 0x83 L(4) value
-    b"\x83\x04\x00\x00\x00\x01"
-    # smpSync 0x85 L(1) value
-    b"\x85\x01\x00"
-    # Sequence of Data 0x87 L(64) Dataset 8 CH
-    # (4 bytes sample + 4 bytes quality)
-    b"\x87\x40"
-    # b"\x00\x00\x00\x01\x00\x00\x00\x00"
-    # b"\x00\x00\x00\x02\x00\x00\x00\x00"
-    # b"\x00\x00\x00\x03\x00\x00\x00\x00"
-    # b"\x00\x00\x00\x04\x00\x00\x00\x00"
-    # b"\x00\x00\x00\x05\x00\x00\x00\x00"
-    # b"\x00\x00\x00\x06\x00\x00\x00\x00"
-    # b"\x00\x00\x00\x07\x00\x00\x00\x00"
-    # b"\x00\x00\x00\x08\x00\x00\x00\x00"
-)
-
-TS_OFFSET = 0
-APP_ID_OFFSET = 0x1E + vlan_size
-SV_ID_OFFSET = 0x31 + vlan_size
-SMP_CNT_OFFSET = SV_ID_OFFSET + 2 + len(svIDFirst)
+def build_asdu(svID, smpCnt, sample_data):
+    """Build a single ASDU (tag 0x30) TLV containing SvID, SmpCnt, ConfRev,
+    SmpSynch and the sequence of data samples."""
+    svid_tlv = b"\x80" + bytes([len(svID)]) + svID.encode("ascii")
+    smpcnt_tlv = b"\x82\x02" + struct.pack(">H", smpCnt)
+    seq_of_data_tlv = b"\x87" + ber_length(len(sample_data)) + sample_data
+    content = svid_tlv + smpcnt_tlv + CONF_REV_TLV + SMP_SYNC_TLV + seq_of_data_tlv
+    return b"\x30" + ber_length(len(content)) + content
 
 
-pcap_data = bytearray()
+def build_savpdu(asdus):
+    """Build the savPDU (tag 0x60): NumOfASDU + SeqOfASDU (containing all
+    the ASDUs passed in)."""
+    seq_content = b"".join(asdus)
+    num_of_asdu_tlv = b"\x80\x01" + bytes([len(asdus)])
+    seq_of_asdu_tlv = b"\xa2" + ber_length(len(seq_content)) + seq_content
+    content = num_of_asdu_tlv + seq_of_asdu_tlv
+    return b"\x60" + ber_length(len(content)) + content
 
-# Add header
-pcap_data += HEADER
 
-sv_data = bytearray(SV_DATA)
-ts = 0
+def build_sv_pdu(app_id, savpdu):
+    """Build the SV common header (AppID, Length, Reserved1, Reserved2)
+    followed by the savPDU. Length covers the whole SV PDU (itself
+    included)."""
+    total_length = 8 + len(savpdu)  # AppID(2)+Length(2)+Reserved1(2)+Reserved2(2)+savPDU
+    header = (
+        struct.pack(">H", app_id)
+        + struct.pack(">H", total_length)
+        + b"\x00\x00\x00\x00"  # Reserved1 & Reserved2
+    )
+    return header + savpdu
+
+
+def build_samples(i, sampling_rate):
+    """Compute the 64 bytes (8 channels x (4 bytes sample + 4 bytes
+    quality)) sequence of data for sample index i."""
+    data = bytearray()
+    for index, channel in enumerate(current_channels):
+        if channel != "In":
+            signal = int(
+                scale_factor_amps
+                * i_rms
+                * np.sqrt(2)
+                * np.sin(
+                    (2 * np.pi * freq * i / sampling_rate)
+                    + ((2 * np.pi / 3) * index)
+                )
+            )
+        else:
+            signal = 0
+        data += signal.to_bytes(4, byteorder="big", signed=True)
+        data += b"\x00\x00\x00\x00"
+    for index, channel in enumerate(voltage_channels):
+        if channel != "Vn":
+            signal = int(
+                scale_factor_volts
+                * v_rms
+                * np.sqrt(2)
+                * np.cos(
+                    (2 * np.pi * freq * i / sampling_rate)
+                    + ((2 * np.pi / 3) * index)
+                )
+            )
+        else:
+            signal = 0
+        data += signal.to_bytes(4, byteorder="big", signed=True)
+        data += b"\x00\x00\x00\x00"
+    return bytes(data)
+
+
+def build_frame(app_id, asdus):
+    """Build a full pcap record (record header + Ethernet frame carrying
+    the SV PDU with the given list of ASDUs), timestamp not yet set."""
+    savpdu = build_savpdu(asdus)
+    sv_pdu = build_sv_pdu(app_id, savpdu)
+    eth_frame = mac_dest + mac_source + vlan_header + ETHERTYPE_SV + sv_pdu
+    return eth_frame
+
+
 samples_per_cyle = 80
 sampling_rate = samples_per_cyle * freq
 scale_factor_amps = 1000
 scale_factor_volts = 100
 voltage_channels = ["Va", "Vb", "Vc", "Vn"]
 current_channels = ["Ia", "Ib", "Ic", "In"]
+
+# Group the streams into chunks of nb_asdu streams: each chunk becomes one
+# frame containing nb_asdu ASDUs (the last chunk may contain fewer ASDUs if
+# nb_streams is not a multiple of nb_asdu).
+stream_ids = list(range(start_id, start_id + nb_streams))
+groups = [stream_ids[k : k + nb_asdu] for k in range(0, len(stream_ids), nb_asdu)]
+
+pcap_data = bytearray()
+pcap_data += PCAP_GLOBAL_HEADER
+
+ts = 0
 for i in range(0, max_counter):
     (second, microsecond) = get_second_microsecond(ts)
     ts = (i + 1) / sampling_rate
-    # Write AppID
-    write_bytes_be(
-        sv_data, APP_ID_OFFSET, struct.pack("H", app_id)
-    )
-    # Write cmpCnt
-    write_bytes_be(
-        sv_data, SMP_CNT_OFFSET, struct.pack("H", (i % int(sampling_rate)))
-    )
-    # Write TS add a +1 offset to second and microsecond to avoid a tcpreplay
-    # limitation. tcpreplay do not support frames with a 0 timestamp.
-    write_bytes_le(
-        sv_data, TS_OFFSET, struct.pack("II", second + 1, microsecond + 1)
-    )
-    for st in range(0, nb_streams):
-        # svID string are svID_prefixXXXX
-        svID_data = f"{svID_prefix}{start_id+st:0{nb_digits}d}".encode("ascii")
-        write_bytes_le(sv_data, SV_ID_OFFSET, svID_data, len(svID_data) - 1)
-        pcap_data += sv_data
-        for index, channel in np.ndenumerate(current_channels):
-            # 4 bytes of sample
-            if channel != "In":
-                signal = int(
-                    scale_factor_amps
-                    * i_rms
-                    * np.sqrt(2)
-                    * np.sin(
-                        (2 * np.pi * freq * i * (1 / sampling_rate))
-                        + ((2 * np.pi / 3) * index[0])
-                    )
-                )
-            else:
-                signal = 0
-            pcap_data += bytearray(
-                signal.to_bytes(4, byteorder="big", signed=True)
-            )
-            # 4 bytes of quality
-            pcap_data += bytearray(b"\x00\x00\x00\x00")
-        for index, channel in np.ndenumerate(voltage_channels):
-            # 4 bytes of sample
-            if channel != "Vn":
-                signal = int(
-                    scale_factor_volts
-                    * v_rms
-                    * np.sqrt(2)
-                    * np.cos(
-                        (2 * np.pi * freq * i * (1 / sampling_rate))
-                        + ((2 * np.pi / 3) * index[0])
-                    )
-                )
-            else:
-                signal = 0
-            pcap_data += bytearray(
-                signal.to_bytes(4, byteorder="big", signed=True)
-            )
-            # 4 bytes of quality
-            pcap_data += bytearray(b"\x00\x00\x00\x00")
+    smpCnt = i % int(sampling_rate)
+
+    # Samples are identical across all streams/ASDUs at a given sample
+    # index, so compute them once per loop iteration.
+    sample_data = build_samples(i, sampling_rate)
+
+    for group in groups:
+        asdus = []
+        for st in group:
+            svID = f"{svID_prefix}{st:0{nb_digits}d}"
+            asdus.append(build_asdu(svID, smpCnt, sample_data))
+
+        eth_frame = build_frame(app_id, asdus)
+        frame_len = len(eth_frame)
+
+        # pcap per-packet record header:
+        # ts_sec(4) ts_usec(4) incl_len(4) orig_len(4), all little-endian.
+        # +1 offset on second/microsecond to avoid a tcpreplay limitation:
+        # tcpreplay does not support frames with a 0 timestamp.
+        record_header = struct.pack(
+            "<IIII", second + 1, microsecond + 1, frame_len, frame_len
+        )
+
+        pcap_data += record_header
+        pcap_data += eth_frame
 
 with open(final_pcap, "wb") as f:
     f.write(pcap_data)
